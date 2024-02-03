0af9437b8b20310697edd4c57a98974040e3ed4c
Franck <157825093+codeWithFranck@users.noreply.github.com>
2024-02-03 11:30:20 -0500

    Initial Commit

---
+++ CheckboxesOfServices/CheckboxesOfServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckboxesOfServicesApp.swift
+//  CheckboxesOfServices
+//
+//  Created by Franck Dalin Mbeutcha Tchuisseu on 2024-02-03.
+//
+
+import SwiftUI
+
+@main
+struct CheckboxesOfServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
